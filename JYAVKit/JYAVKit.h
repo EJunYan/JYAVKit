d70a912078de312c0aade56d2576103f0a5c1e38
EJunYan <longjunyan913@gmail.com>
2017-12-29 15:46:32 +0800

    Initial Commit

---
+++ JYAVKit/JYAVKit.h
@@ -0,0 +1,19 @@
+//
+//  JYAVKit.h
+//  JYAVKit
+//
+//  Created by LongFu on 2017/12/29.
+//  Copyright © 2017年 onelcat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JYAVKit.
+FOUNDATION_EXPORT double JYAVKitVersionNumber;
+
+//! Project version string for JYAVKit.
+FOUNDATION_EXPORT const unsigned char JYAVKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JYAVKit/PublicHeader.h>
+
+
